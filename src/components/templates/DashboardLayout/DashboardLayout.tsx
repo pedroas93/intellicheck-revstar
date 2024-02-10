ad660e04ab8eda149f2e8472e657f25913434876
David Pedraza <david.pedraza@javeriana.edu.co>
2024-02-10 11:00:37 -0500

    chore: Fix Git hooks for pnpm

--- src/components/templates/DashboardLayout/DashboardLayout.tsx
+++ src/components/templates/DashboardLayout/DashboardLayout.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import type { FC } from 'react';
 
-import { TableComponent } from '../../components/UserTable/UserTable';
+// import { TableComponent } from '../../components/UserTable/UserTable';
 
 export const Admin: FC = () => {
   return (
     <div className="flex w-full bg-white justify-center   rounded-3xl h-[100vw] ">
       <div className="flex w-full bg-white justify-center flex-wrap rounded-3xl">
-        <TableComponent />
+        {/* <TableComponent /> */}
         {/* <span style={{ color: "red" }}>HELLO???</span> */}
       </div>
     </div>
